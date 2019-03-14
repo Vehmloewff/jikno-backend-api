f5204eaa1a7486a4fe1ab73477298b7232091d8e
Elisarmor <ejsisgreat@gmail.com>
2019-03-14 15:12:38 -0500

    Add missing "."

--- actions/email_user.php
+++ actions/email_user.php
@@ -2,7 +2,7 @@
 if ($email && $content && $subject) {
     // To send HTML mail, the Content-type header must be set
     $headers = "From: jiknobot@jikno.com\r\n";
-    $headers = "MIME-Version: 1.0\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 
     // send email

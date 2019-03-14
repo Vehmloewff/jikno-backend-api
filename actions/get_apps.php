1511e536a159d2ae613a9c9feb987ae867a6cdf4
Elisarmor <ejsisgreat@gmail.com>
2019-03-14 17:49:16 -0500

    Commit

--- actions/get_apps.php
+++ actions/get_apps.php
@@ -5,14 +5,25 @@ if ($email && $password) {
 
     if ($result->num_rows == 1) {
         while($row = $result->fetch_assoc()) {
-            $content = $row["content"];
+            $apps = json_decode($row["content"]);
         }
 
-        
 
+        $obj = new StdClass;
+        foreach ($apps as $branch => $branch_data) {
+            $used_app = false;
+            foreach($branch_data->users as $user) {
+                if ($user == $email) {
+                    $used_app = true;
+                }
+            }
 
+            if ($used_app) {
+                $obj[$branch] = $branch_data;
+            }
+        }
 
-
+        responseBuilder(false, $obj, "OK");
 
 
 

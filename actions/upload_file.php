0e3c4ca6d3751de5bdeaf13fdb15e8e03eed834f
Elisarmor <ejsisgreat@gmail.com>
2019-04-22 14:34:15 -0500

    Return JSON

--- actions/upload_file.php
+++ actions/upload_file.php
@@ -1,11 +1,10 @@
 <?php
-echo "<p>" . $_POST['csv_file'] . " => file input successfull</p>";
 $target_dir = "cloud/avatars/";
 $file_name = $_FILES['file']['name'];
 $file_tmp = $_FILES['file']['tmp_name'];
 
 if (move_uploaded_file($file_tmp, $target_dir . $file_name)) {
-    echo "<h1>File Upload Success</h1>";
+    responseBuilder(false, "File uploaded", "OK");
 } else {
-    echo "<h1>File Upload not successfull</h1>";
+    responseBuilder(true, "Error in file upload!", "FAILED");
 }
